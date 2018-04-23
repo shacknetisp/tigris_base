223d91d987d0727c219b185fae12db8940412c60
Beha <shacknetisp@mail.com>
2018-04-23 10:30:08 -0400

    Simpler damage.

--- damage.lua
+++ damage.lua
@@ -6,21 +6,15 @@ m.handlers = {}
 
 -- Damage player by table of damage types.
 function m.apply(obj, damage)
-    -- Process damage and convert to fleshy.
-    local armor = obj:get_armor_groups()
-    local total_fleshy = 0
+    -- Sum basic damage from handlers.
+    local total = 0
     for k,v in pairs(damage) do
         assert(m.damage_handlers[k])
-        v = m.damage_handlers[k](obj, v)
-
-        -- Ignore armor (fleshy handler will handle actual fleshy damage).
-        local factor = (armor.fleshy or 100) / 100
-        if factor > 0 then
-            total_fleshy = total_fleshy + v / factor
-        end
+        total = total + m.damage_handlers[k](obj, v)
     end
 
-    obj:punch(obj, 1.0, {full_punch_interval=1.0, damage_groups={fleshy=total_fleshy}, nil})
+    -- Apply basic damage.
+    obj:set_hp(obj:get_hp() - total)
 end
 
 tigris.damage.register("fleshy", function(obj, value)

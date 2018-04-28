55fc01f31b5f4b3ed33f1e1be6016c749aea7e3e
Beha <shacknetisp@mail.com>
2018-04-28 13:14:36 -0400

    tigris.include_dir()

--- init.lua
+++ init.lua
@@ -4,6 +4,13 @@ function tigris.include(p)
     dofile(minetest.get_modpath(minetest.get_current_modname()) .. "/" .. p)
 end
 
+function tigris.include_dir(d)
+    local mp = minetest.get_modpath(minetest.get_current_modname()) .. "/" .. d
+    for _,entry in ipairs(minetest.get_dir_list(mp, false)) do
+        tigris.include(d .. "/" .. entry)
+    end
+end
+
 -- Danger level increases by 1 every <x> distance from 0,0,0.
 local start = minetest.string_to_pos(minetest.settings:get("tigris.world_center")) or vector.new(0, 0, 0)
 local scale = tonumber(minetest.settings:get("tigris.level_scale")) or 500

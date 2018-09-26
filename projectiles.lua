738c647273e157144a0f31745986d75103ebf0ab
Beha <shacknetisp@mail.com>
2018-09-26 14:05:09 -0400

    Map-loading projectiles.

--- projectiles.lua
+++ projectiles.lua
@@ -16,6 +16,29 @@ function tigris.register_projectile(name, def)
 
         _created = 0,
 
+        on_activate = function(self, static_data)
+            if static_data and static_data ~= "" then
+                for k,v in pairs(minetest.deserialize(static_data)) do
+                    self[k] = v
+                end
+                if self._forceload then
+                    minetest.forceload_free_block(self._forceload)
+                    self._forceload = nil
+                end
+            end
+        end,
+
+        get_staticdata = function(self)
+            local t = {}
+            for k,v in pairs(self) do
+                -- Only save custom properties.
+                if k:sub(1,1) == "_" then
+                    t[k] = v
+                end
+            end
+            return minetest.serialize(t)
+        end,
+
         on_step = function(self, dtime)
             local alive = os.time() - self._created
             if alive > (self.timeout_override or timeout) then
@@ -26,6 +49,8 @@ function tigris.register_projectile(name, def)
                 return
             end
 
+            print(alive, minetest.pos_to_string(self.object:getvelocity()), minetest.pos_to_string(self.object:getpos()))
+
             local pos = self.object:getpos()
             local diff = vector.new(pos.x - self._last_pos.x, pos.y - self._last_pos.y, pos.z - self._last_pos.z)
             local dir = vector.apply(diff, function(a) return (a / math.abs(a)) * 0.25 end)
@@ -35,13 +60,37 @@ function tigris.register_projectile(name, def)
             for z=self._last_pos.z,pos.z,dir.z do
                 local point = vector.new(x, y, z)
                 local node = minetest.get_node(point)
+
                 if node.name == "ignore" then
-                    self.object:remove()
-                    return
+                    if def.load_map then
+                        VoxelManip():read_from_map(point, point)
+                        node = minetest.get_node(point)
+                        -- Still not available. Try emerging if within limits.
+                        if node.name == "ignore" then
+                            local ok = true
+                            for _,c in ipairs{"x", "y", "z"} do
+                                if point[c] < tigris.world_limits.min[c] or point[c] > tigris.world_limits.max[c] then
+                                    ok = false
+                                end
+                            end
+                            if ok then
+                                minetest.emerge_area(point, point, function()
+                                    self._forceload = point
+                                    minetest.forceload_block(self._forceload)
+                                end)
+                            else
+                                self.object:remove()
+                            end
+                            return
+                        end
+                    else
+                        self.object:remove()
+                        return
+                    end
                 end
 
                 if node.name == "air" then
-                    self.last_air = vector.round(point)
+                    self._last_air = vector.round(point)
                 else
                     if def.on_any_hit and def.on_any_hit(self, point) then
                         self.object:remove()
